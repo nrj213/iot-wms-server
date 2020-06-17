c45f7c64697817c5042dca40f20625452e3fc0b2
JUSTIN, ANURAJ <justin.anuraj@nissanmotor.com>
2020-06-17 23:39:17 +0530

    Modified attribute name in user detail GET response

--- dao/staff.dao.js
+++ dao/staff.dao.js
@@ -7,18 +7,18 @@ const { MessageCodes } = require('../utils/constants')
 const { Exception } = require('../payload/exception')
 
 exports.findStaffById = (staffId) => {
-    const query = `SELECT TOP 1000 [staff_id]
-                                    ,staff.[name]
-                                    ,[address]
-                                    ,[mobile_no] as mobileNo
-                                    ,staff.[area_id] as areaId
-                                    ,area.[name] as areaName
-                                    ,[date_of_joining] as dateOfJoining
-                                    ,[date_of_leaving] as dateOfLeaving
-                                FROM [wms].[wmsuser].[staff_details] staff
-                                JOIN [wms].[wmsuser].[area] area
-                                ON staff.area_id = area.area_id
-                                WHERE staff.staff_id = ${staffId}`
+    const query = `SELECT [staff_id] as staffId
+                        ,staff.[name]
+                        ,[address]
+                        ,[mobile_no] as mobileNo
+                        ,staff.[area_id] as areaId
+                        ,area.[name] as areaName
+                        ,[date_of_joining] as dateOfJoining
+                        ,[date_of_leaving] as dateOfLeaving
+                    FROM [wms].[wmsuser].[staff_details] staff
+                    JOIN [wms].[wmsuser].[area] area
+                    ON staff.area_id = area.area_id
+                    WHERE staff.staff_id = ${staffId}`
 
     return new Promise((resolve, reject) => {
         db.executeQuery(query)
